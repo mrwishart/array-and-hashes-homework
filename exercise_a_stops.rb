b5e4b414fe912eae08c1dcd4dce5133c61785992
Paul Wishart <misterwishart@gmail.com>
2018-10-17 16:18:34 +0100

    Completed up to Q8 on Exercise A

--- exercise_a_stops.rb
+++ exercise_a_stops.rb
@@ -14,7 +14,7 @@ stops.insert(4, "Polmont")
 
 # 4. Work out the index position of `"Linlithgow"`
 
-p "Linlithgow is at index position:  #{stops.index("Linlithgow")}"
+p "Linlithgow is at index position: #{stops.index("Linlithgow")}"
 
 # 5. Remove `"Livingston"` from the array using its name
 
@@ -26,8 +26,25 @@ cumbernauld_index = stops.index("Cumbernauld")
 stops.delete_at(cumbernauld_index)
 
 # 7. How many stops there are in the array?
+
+p "There are #{stops.length()} stops in the array"
+
 # 8. How many ways can we return `"Falkirk High"` from the array?
+
+puts ""
+p "Various ways to return Falkirk High:"
+puts ""
+p stops[2]
+p stops[-5]
+p stops.at(2)
+p stops[stops.index("Falkirk High")]
+p stops.select {|a| a == "Falkirk High"}
+p stops.reject {|a| a != "Falkirk High"}
+
 # 9. Reverse the positions of the stops in the array
+
+
+
 # 10. Print out all the stops using a for loop
 
 p stops
